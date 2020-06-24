5ea7e559d56b33878372c5517f3e475e81d2de97
hideo54 <hideo54@hideo54.com>
2020-06-25 03:27:08 +0900

    Improve accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,10 @@ const Gallery = () => {
         return (
             <li className='tile'>
                 <a href={`https://twitter.com/${account.screenName}`} target='_blank' rel='noopener' >
-                    <img src={account.iconImageUrl} loading='lazy' />
-                    <p className={textType === 'followerCount' ? 'follower-count' : undefined}>{text}</p>
+                    <img src={account.iconImageUrl} alt={account.screenName} loading='lazy' />
+                    {text &&
+                        <p className={textType === 'followerCount' ? 'follower-count' : undefined}>{text}</p>
+                    }
                 </a>
             </li>
         );
