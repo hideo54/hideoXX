467424f4fbf0e0ade68bb8e22697770706d3db2d
hideo54 <hideo54@hideo54.com>
2020-06-24 23:57:02 +0900

    Make gallary view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import data from '../data.json';
 
 const App = () => {
+    const tiles = data.map(account => (
+        <li>
+            <img src={`icons/${account.screen_name.toLowerCase()}.jpg`} />
+        </li>
+    ));
     return (
-        <p>Hello, world!</p>
+        <>
+            <header>
+                <h1>hideoXX</h1>
+                <p>Find your favorite hideoXX!</p>
+            </header>
+            <ul className='gallary'>
+                {tiles}
+            </ul>
+        </>
     );
 };
 
