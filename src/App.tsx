15abbf50b829dcc3ae1e1ec1d87be0779ca16616
hideo54 <hideo54@hideo54.com>
2020-06-25 00:21:44 +0900

    Add shuffle feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+import { shuffle } from 'lodash';
 import data from '../data.json';
 
-const App = () => {
-    const tiles = data.map(account => (
+const Gallery = () => {
+    const [ isRandom, setIsRandom ] = useState<boolean>(false);
+    let order = data.map(account => account.screen_name.toLowerCase());
+    const handleIsRandomChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setIsRandom(e.target.checked);
+    };
+    if (isRandom) order = shuffle(order);
+    const tiles = order.map(name => (
         <li>
-            <img src={`icons/${account.screen_name.toLowerCase()}.jpg`} />
+            <img src={`icons/${name}.jpg`} />
         </li>
     ));
+    return (
+        <>
+            <div id='controls'>
+                <input type='checkbox'
+                    id='is-random'
+                    name='is-random'
+                    checked={isRandom}
+                    onChange={handleIsRandomChange}
+                />
+                <label htmlFor='is-random'>Shuffle</label>
+            </div>
+            <ul className='gallary'>
+                {tiles}
+            </ul>
+        </>
+    );
+};
+
+const App = () => {
     return (
         <>
             <header>
                 <h1>hideoXX</h1>
                 <p>Find your favorite hideoXX!</p>
             </header>
-            <ul className='gallary'>
-                {tiles}
-            </ul>
+            <Gallery />
         </>
     );
 };
